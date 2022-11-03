84d5a5fed0d49b948db876a18566f8e60ae7c891
nareshdavid <111025551+nareshdavid@users.noreply.github.com>
2022-11-03 10:21:14 -0500

    Add files via upload

---
+++ JavaScript/semana 15/JS/Funcion apostar.js
@@ -0,0 +1,15 @@
+function apostar(){
+    var a=Math.round(Math.random()*10);
+    document.getElementById("resultado").value=a;
+    var b=document.getElementById("apostado").value;
+    if (a==b){
+        document.getElementById("salida").value="ganó";
+} else {
+        document.getElementById("salida").value="perdió";
+}
+}
+    function Cancel(){
+    document.getElementById("apostado").value=" ";
+    document.getElementById("resultado").value=" ";
+    document.getElementById("salida").value=" ";
+}
